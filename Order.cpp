24a9cea0c1c25db2888ad1e138e725faee6f3eac
KennyLuong02 <126566057+KennyLuong02@users.noreply.github.com>
2023-05-10 14:20:03 +0930

    Update Order.cpp
    Change the file

--- Order.cpp
+++ Order.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
 #include "Order.h"
 #include "Bowl.h"
 
@@ -8,15 +9,78 @@ using namespace std;
 
 // Need some more work
 
-void Order::putOrder() {
-    //This can be randomised later in the game but it will be hard-code for now
 
-    order[5] = {15, 21, 33, 23, 21};
+Order::Order() {
+    //This can be randomised later in the game but it will be hard-code for now
+    orderSize = 5;
+    order[orderSize] = {15, 21, 33, 23, 21};
+
+    counter = 0;
 }
 
-void putOrder();
-        void checkOrder();
-        int receiveOrder(int content);
+void Order::putOrder() {
+    //This for-loop can be use again later and should be fine
+    for (int i = 0; i < orderSize; i++) {
+        if (order[i] == 15) {
+            cout << "Mushroom soup: include 3 mushrooms" << endl;
+        } else if (order[i] == 21) {
+            cout << "Onion soup: include 3 onions" << endl;
+        } else if (order[i] == 33) {
+            cout << "Tomato soup: include 3 tomatoes" << endl;
+        } else if (order[i] == 23) {
+            cout << "Combination soup: include 1 of each veges" << endl;
+        }
 
-int order;
-        int contentOfOrder;
+        //To check for bug later
+        else {cout << "An error is here in the Order" << endl; break;}
+
+        counter++;
+        sleep(10);
+    }
+
+};
+
+void Order::checkOrder() {
+    for (int i = 0; i < counter; i++) {
+        if (order[i] == 15) {
+            cout << "Mushroom soup: include 3 mushrooms" << endl;
+        } else if (order[i] == 21) {
+            cout << "Onion soup: include 3 onions" << endl;
+        } else if (order[i] == 33) {
+            cout << "Tomato soup: include 3 tomatoes" << endl;
+        } else if (order[i] == 23) {
+            cout << "Combination soup: include 1 of each veges" << endl;
+        }
+    }
+};
+
+int Order::receiveOrder(Bowl* theBowl) { //The content is received from the Bowl
+    contentOfBowl = theBowl->getContent(); //Get the bowl content
+    if (contentOfBowl == 0) {
+        theBowl->setState(-1);
+        return 0; //check if there is something else to return back 
+    }
+    for (int i = 0; i < counter; i++) {
+        if (order[i] == contentOfBowl) {
+            order[i] = NULL;
+            theBowl->setState(-1);
+            break;
+
+        } else if (order[i] == contentOfBowl) {
+            order[i] = NULL;
+            theBowl->setState(-1);
+            break;
+
+        } else if (order[i] == contentOfBowl) {
+            order[i] = NULL;
+            theBowl->setState(-1);
+            break;
+
+        } else if (order[i] == contentOfBowl) {
+            order[i] = NULL;
+            theBowl->setState(-1);
+            break;
+
+        }
+    }
+};
