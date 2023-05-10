56f05e7b7b745bc48eebb75d1d25918163be718c
anshbirani <113323051+anshbirani@users.noreply.github.com>
2023-05-10 16:30:31 +0930

    Update Resources.cpp

--- Resources.cpp
+++ Resources.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+//Need some more work
+
 Resources::Resources(): onion(100), tomato(100), mushroom(100) {
     bowlAmount = new Bowl*[3];
 };
@@ -13,3 +15,13 @@ void Resources::removeOnion() {onion = onion - 1;};
 void Resources::removeTomato() {tomato = tomato - 1;};
 
 void Resources::removeMushroom() {mushroom = mushroom -1;};
+
+int Resources::getOnion() {
+    return onion;
+}
+int Resources::getTomato() {
+    return tomato;
+}
+int Resources::getMushrooom() {
+    return mushroom;
+}
