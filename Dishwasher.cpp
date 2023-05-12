e278b1cc72a4ba6394af299796545dca9fbe3287
KennyLuong02 <126566057+KennyLuong02@users.noreply.github.com>
2023-05-12 17:40:18 +0930

    Update Dishwasher.cpp
    Kenny

--- Dishwasher.cpp
+++ Dishwasher.cpp
@@ -22,18 +22,26 @@ void Dishwasher::setPowerState(bool n) {
     isOn = n;
  };
 
-void Dishwasher::timerFunction(int n) { //need a Bowl input pointer
+
+void Dishwasher::timerFunction(Bowl* theBowl) { //need a Bowl input pointer
     //Check if the Dishwasher is empty
     if (state == 0) {
         //Wait 2s before make the bowl clean
+        timeContainer = time(NULL);
+
+        //If they Player want o access, deny
         time_t timeNow;
         timeNow = time(NULL) - timeContainer;
+        cout << "Dishwashing in progress" << endl;
 
         while (timeNow < 2) {
             timeNow = time(NULL) - timeContainer;
+            state = 1;
         }
         
+        
         //Need a pointer to Bowl to change the state to dirty
+        theBowl->setState(0);
 
     } else if (state == 1) {
         cout << "Dishwasher is full" << endl;
