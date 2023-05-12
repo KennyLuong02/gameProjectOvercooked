80386772245f2d1de816cd8dcb19715be894e137
KennyLuong02 <126566057+KennyLuong02@users.noreply.github.com>
2023-05-12 19:31:46 +0930

    Create testContainer.cpp
    Kenny

---
+++ testContainer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "Player.h"
+#include "Container.h"
+
+using namespace std;
+
+int main() {
+    Container c1;
+    cout << c1.getState()<< endl;
+    
+    // int getState();
+    //     void setState(int n);
+    //     int getContent();
+    //     void setContent(int content);
+
+    //     void timerFunction(int contentToAdd);
+
+
+
+
+}
