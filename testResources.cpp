f956992cd35df6a07ba6245c9236ce9b59471d16
KennyLuong02 <126566057+KennyLuong02@users.noreply.github.com>
2023-05-12 19:32:29 +0930

    Create testResources.cpp
    Kenny

---
+++ testResources.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "Player.h"
+#include "Resources.h"
+
+using namespace std;
+
+int main() {
+    Resources resources1;
+    cout << resources1.getOnion()<< endl;
+    
+
+
+
+
+}
