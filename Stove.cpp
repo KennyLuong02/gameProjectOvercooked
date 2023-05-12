aee3ab93482e4c0baac8ad7b729895b44f3f81ac
KennyLuong02 <126566057+KennyLuong02@users.noreply.github.com>
2023-05-12 19:22:22 +0930

    Update Stove.cpp

--- Stove.cpp
+++ Stove.cpp
@@ -27,22 +27,24 @@ void Stove::setContent(int content) {
 
 
 void Stove::timerFunction(int contentToAdd) { //need to add input of the content as addContent()
-    content = content + contentToAdd;
     //Check if the sum is higher than 14
-    if (content > 14) {
+    if ((content = content + contentToAdd) > 14) {
         //Wait 3s before release the content
+        timeContainer = time(NULL);
+
         time_t timeNow;
-        timeNow = time(NULL);
-        timeNow = timeNow - timeContainer;
+        timeNow = time(NULL) - timeContainer;
         cout << "Cooking in progress" << endl;
 
         while (timeNow < 3) {
-            timeNow = time(NULL);
-            timeNow = timeNow - timeContainer;
+            timeNow = time(NULL) - timeContainer;
         }
 
         //Make the content to the soup sum
         //Use getContent to put soup in Bowl
         //If the Bowl get the content then setContent(0)
+    } else if ((content = content + contentToAdd) > 33) {
+        cout << "The stove is full" << endl;
+        //may need 
     }
 };
