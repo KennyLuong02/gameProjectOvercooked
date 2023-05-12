7207c97f0d048effe5a8d948f27fcd49772fa327
KennyLuong02 <126566057+KennyLuong02@users.noreply.github.com>
2023-05-12 19:32:50 +0930

    Create testStove.cpp
    Kenny

---
+++ testStove.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include "Player.h"
+#include "Container.h"
+#include "Stove.h"
+
+using namespace std;
+
+int main() {
+    Stove s1;
+    cout << s1.getContent()<< endl;
+    
+    // int getState();
+    //     void setState(int n);
+    //     int getContent();
+    //     void setContent(int content);
+
+    //     void timerFunction(int contentToAdd);
+
+
+
+
+}
