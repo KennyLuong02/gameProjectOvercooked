80be93f50a18d2d913b2ba1060847eaad79625b7
KennyLuong02 <126566057+KennyLuong02@users.noreply.github.com>
2023-05-12 19:15:07 +0930

    Update Resources.cpp
    Kenny has changed this

--- Resources.cpp
+++ Resources.cpp
@@ -6,15 +6,22 @@ using namespace std;
 
 //Need some more work
 
+// Resources::Resources(): onion(100), tomato(100), mushroom(100) {
+//     bowlAmount = new Bowl*[3];
+// };
+
 Resources::Resources(): onion(100), tomato(100), mushroom(100) {
+    mushroom = 100;
+    onion = 100;
+    tomato = 100;
     bowlAmount = new Bowl*[3];
 };
 
-void Resources::removeOnion() {onion = onion - 1;};
+// void Resources::removeOnion() {onion = onion - 1;};
 
-void Resources::removeTomato() {tomato = tomato - 1;};
+// void Resources::removeTomato() {tomato = tomato - 1;};
 
-void Resources::removeMushroom() {mushroom = mushroom -1;};
+// void Resources::removeMushroom() {mushroom = mushroom -1;};
 
 int Resources::getOnion() {
     return onion;
@@ -25,3 +32,18 @@ int Resources::getTomato() {
 int Resources::getMushrooom() {
     return mushroom;
 }
+
+void Resources::pickUp(string pickUpInput) {
+    if (pickUpInput == "Bowl") {
+        setPlayerState(1);
+    } else if (pickUpInput == "Mushroom") {
+        setPlayerState(5);
+        mushroom = mushroom -1;
+    } else if (pickUpInput == "Onion") {
+        setPlayerState(7);
+        onion = onion - 1;
+    } else if (pickUpInput == "Tomato") {
+        setPlayerState(11);
+        tomato = tomato - 1;
+    }
+};
